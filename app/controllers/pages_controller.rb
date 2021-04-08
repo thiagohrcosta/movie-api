a971957bf71f14dd3aa4342a16fed07df2ea65d5
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-08 11:44:33 -0300

    Implement the api connection and response

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
+require 'open-uri'
+
 class PagesController < ApplicationController
   def index
+
+    my_key = "553ada5dbd7bc2b635e9495e52ec1f45"
+    movie = 300
+    response = open("https://api.themoviedb.org/3/movie/#{movie}?api_key=#{my_key}")
+    @my_json = JSON.parse(response.read)
+    @movie_title = @my_json.entries[9]
+    raise
+  end
+
+  def display(movie)
   end
 end
