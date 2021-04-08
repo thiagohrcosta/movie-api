7c09293052d7c64ca7de1366773004af77dc344b
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-08 14:21:36 -0300

    Display poster on movie screen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,11 +12,17 @@ class PagesController < ApplicationController
     # raise
 
     # movie must be a number
-    response = open("https://api.themoviedb.org/3/search/movie?api_key=#{my_key}&query=#{movie}")
+    response = open("https://api.themoviedb.org/3/search/movie?api_key=#{my_key}&query=#{movie}&page=1&include_adult=false")
     @my_json = JSON.parse(response.read)
+    my_poster = @my_json.entries[1][1][1]["poster_path"]
+
+    @my_link = "https://image.tmdb.org/t/p/w500/#{my_poster}"
     # @movie_title = @my_json.entries[9]
-    raise
+    # raise
 
     # https://api.themoviedb.org/3/search/movie?api_key=#{my_key}&query=#{movie}
+
+    # responseMatrix = open("https://api.themoviedb.org/3/movie/603?api_key=553ada5dbd7bc2b635e9495e52ec1f45")
+    # @my_Matrix = JSON.parse(responseMatrix.read)
   end
 end
