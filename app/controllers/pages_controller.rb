8ac381dac8d684af6cdcc17991f996df22a2a325
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-08 13:46:32 -0300

    Display the JSON info on screen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,21 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   def index
-
-    my_key = "553ada5dbd7bc2b635e9495e52ec1f45"
-    movie = 300
-    response = open("https://api.themoviedb.org/3/movie/#{movie}?api_key=#{my_key}")
-    @my_json = JSON.parse(response.read)
-    @movie_title = @my_json.entries[9]
-    raise
+    #raise
   end
 
-  def display(movie)
+  def display
+    @answer = params[:title]
+    my_key = "553ada5dbd7bc2b635e9495e52ec1f45"
+    movie = @answer
+    # raise
+
+    # movie must be a number
+    response = open("https://api.themoviedb.org/3/search/movie?api_key=#{my_key}&query=#{movie}")
+    @my_json = JSON.parse(response.read)
+    # @movie_title = @my_json.entries[9]
+    raise
+
+    # https://api.themoviedb.org/3/search/movie?api_key=#{my_key}&query=#{movie}
   end
 end
